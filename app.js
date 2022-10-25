a61e982a533ec2ce3c165f5aa81d8182008ada90
damianhol <holub.damian1@gmail.com>
2022-10-25 12:21:05 +0200

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-import * as THREE from '../../libs/three/three.module.js';
-import { OrbitControls } from '../../libs/three/jsm/OrbitControls.js';
-import { Stats } from '../../libs/stats.module.js';
-import { ARButton } from '../../libs/ARButton.js';
+import * as THREE from './libs/three/three.module.js';
+import { OrbitControls } from './libs/three/jsm/OrbitControls.js';
+import { Stats } from './libs/stats.module.js';
+import { ARButton } from './libs/ARButton.js';
 
 class App{
 	constructor(){
